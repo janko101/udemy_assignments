372f5fa9a7d0c3efc42fe46223cbb3829b0f8d14
Janko <mailodjanka@gmail.com>
2020-05-08 12:57:22 +0200

    Adds a change handlet method and outputs the legth of input in a paragraph below

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ class App extends Component {
   state = {
     userInput: ''
   }
+
+  userInputChangedHandler = (event) => {
+    this.setState({userInput: event.target.value})
+  }
   render() {
     return (
       <div className="App">
         <h2>2nd Assignment</h2>
-        <input type="text" />
-        <p>Length of your input is: </p>
+        <input type="text" onChange={this.userInputChangedHandler} value={this.state.userInput}/>
+        <p>Length of your input is: {this.state.userInput.length} </p>
       </div>
     );
   }
