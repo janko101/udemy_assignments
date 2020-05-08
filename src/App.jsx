a31c9189049036a17d9624e893f222750253e9dd
Janko <mailodjanka@gmail.com>
2020-05-08 12:26:32 +0200

    Adds an input field and a p field which will give the length of input

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    userInput: ''
+  }
   render() {
     return (
       <div className="App">
         <h2>2nd Assignment</h2>
+        <input type="text" />
+        <p>Length of your input is: </p>
       </div>
     );
   }
