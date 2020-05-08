2a7930d28771aab340f3c386905899d1de07a098
Janko <mailodjanka@gmail.com>
2020-05-08 13:39:01 +0200

    Adds a new component Char and applies requested styling

---
+++ src/Components/Char.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Char = () => {
+  const style = {
+    display: "inline-block",
+    padding: "16px",
+    margin: "16px",
+    border: "1px solid purple",
+    boxShadow: "0px 2px 15px purple",
+  };
+  return <div style={style}></div>;
+};
+
+export default Char;
